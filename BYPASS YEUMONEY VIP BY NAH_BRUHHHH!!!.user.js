10bb63275d954528c3c1b394faa8ad1fc8d627df
bruhhhh9150 <thienbropro@gmail.com>
2025-08-17 19:07:52 +0700

    BYPASS YEUMONEY VIP BY NAH_BRUHHHH!!!.user.js

--- BYPASS YEUMONEY VIP BY NAH_BRUHHHH!!!.user.js
+++ BYPASS YEUMONEY VIP BY NAH_BRUHHHH!!!.user.js
@@ -12,6 +12,7 @@
 // @match        https://w88xlm.com/*
 // @match        https://fb88mg.com/*
 // @match        https://fb88dq.com/*
+// @match        https://fb88eo.com/*
 // @match        https://vn88no.com/*
 // @match        https://vn88me.com/*
 // @match        https://vn88ie.com/*
@@ -23,18 +24,24 @@
 // @match        https://bet88ve.com/*
 // @match        https://bet88bt.com/*
 // @match        https://bet88mu.com/*
+// @match        https://bet88lp.com/*
 // @match        https://88betag.com/*
+// @match        https://88bettp.com/*
 // @match        https://165.22.63.250/*
 // @match        https://88betux.com/*
+// @match        https://88betgk.com/*
 // @match        https://88betvb.com/*
 // @match        https://v9betxa.com/*
 // @match        https://v9betho.com/*
 // @match        https://v9betgz.com/*
 // @match        https://v9betlg.com/*
+// @match        https://v9betnl.com/*
 // @match        https://bk8ze.com/*
 // @match        https://bk8ly.com/*
 // @match        https://bk8hn.com/*
+// @match        https://bk8fp.com/*
 // @match        https://bk8xo.com/*
+// @match        https://bk8wx.com/*
 // @run-at       document-end
 // @icon         https://apngasm.sourceforge.net/colorcube_mini.png
 // ==/UserScript==
